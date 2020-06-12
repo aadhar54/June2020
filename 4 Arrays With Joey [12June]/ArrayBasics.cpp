2a8f73e96621b065491e392c7b77c2b2d666d139
react ninja <aadhar54@gmail.com>
2020-06-12 23:54:59 +0530

    Bonus Doc Added for Arrays

--- 4 Arrays With Joey [12June]/ArrayBasics.cpp
+++ 4 Arrays With Joey [12June]/ArrayBasics.cpp
@@ -8,12 +8,12 @@ int main(){
 	//0 ........99 
 
 	a[0]=5;
-	cout<<a[1];
+	cout<<a[1]<<endl; 
 
 
 	int b[5];
 	for(int j=0;j<5;j++){
-
+		cout<<"Enter "<<j<<" element"<<endl;
 		cin>>b[j];  // User Input 
 		cout<<b[j]<<endl;
 	}
